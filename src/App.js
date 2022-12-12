0169935853cbc8c7b65fdf44f1387ad1e016e9e7
abramperr <118059174+abramperr@users.noreply.github.com>
2022-12-12 20:54:17 +0530

    removing 3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
-import Three from '../src/components/Three.jsx';
+
 function App() {
   return (
     <div >
